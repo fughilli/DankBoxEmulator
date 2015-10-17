3ca1f3bff1265049d0dd332786693ecc8a43c7f3
Kevin Balke <kbalke@ucla.edu>
2015-10-17 15:27:03 -0700

    Added comments to main

--- main.c
+++ main.c
@@ -1,21 +1,45 @@
-#include <stdio.h>
+/**
+ * @brief The main routine of the DankBoxEmulator. Reads in a binary file and
+ *        starts executing.
+ * @author Kevin Balke
+ */
+
+#include "device_uart.h"
+#include "devices.h"
 #include "processor.h"
+
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
+    /*
+     * If the user does not provide a binary to run, print the usage and exit.
+     */
     if(argc != 2)
     {
         printf("USAGE:\n\t%s:\t[BINFILE]\n", argv[0]);
         return 1;
     }
+
+    /*
+     * Initialize the processor.
+     */
     proc_init();
 
+    /*
+     * Initialize the UART device.
+     */
+    uart_init();
+
+    /*
+     * Load the program binary from the provided binary file.
+     */
     proc_load_program(argv[1]);
 
-    while(proc_instr_execute(get_mem_word(proc_regs.PC)))
-    {
-//        fgetc(stdin);
-    }
+    /*
+     * Execute the program in a loop.
+     */
+    while(proc_instr_execute(get_mem_word(proc_regs.PC)));
 
     return 0;
 }
